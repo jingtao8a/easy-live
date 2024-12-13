3a06adc62627f15639e9cbabaa6675b982651afb
yuxintao <1921056015@qq.com>
2024-12-13 08:33:06 +0800

    update JsonUtils.java

--- easylive-common/src/main/java/org/jingtao8a/utils/JsonUtils.java
+++ easylive-common/src/main/java/org/jingtao8a/utils/JsonUtils.java
@@ -1,8 +1,13 @@
 package org.jingtao8a.utils;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import lombok.extern.slf4j.Slf4j;
+import java.util.*;
 
+@Slf4j
 public class JsonUtils {
     public static String convertObj2Json(Object obj) {
         if (null == obj) {
@@ -10,4 +15,8 @@ public class JsonUtils {
         }
         return JSON.toJSONString(obj, SerializerFeature.DisableCircularReferenceDetect);
     }
+
+    public static <T> T convertJson2Obj(String json, Class<T> clazz) { return JSONObject.parseObject(json, clazz); }
+
+    public static <T> List<T> convertJson2List(String json, Class<T> clazz) { return JSONArray.parseArray(json, clazz);}
 }
