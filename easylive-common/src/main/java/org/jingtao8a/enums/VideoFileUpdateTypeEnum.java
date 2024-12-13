b91f1da546895c89d0c8b6c540e49151b6c53f4b
yuxintao <1921056015@qq.com>
2024-12-13 08:37:01 +0800

    add VideoFileUpdateTypeEnum.java

---
+++ easylive-common/src/main/java/org/jingtao8a/enums/VideoFileUpdateTypeEnum.java
@@ -0,0 +1,16 @@
+package org.jingtao8a.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum VideoFileUpdateTypeEnum {
+    NO_UPDATE(0, "无更新"),
+    UPDATE(1, "有更新"),
+    ;
+    private Integer status;
+    private String desc;
+    VideoFileUpdateTypeEnum(Integer status, String desc) {
+        this.status = status;
+        this.desc = desc;
+    }
+}
