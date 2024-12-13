9fccd16e05763fb259ec0418d94710e648947b01
yuxintao <1921056015@qq.com>
2024-12-13 08:38:35 +0800

    添加ExecuteQueueTask

---
+++ easylive-web/src/main/java/org/jingtao8a/web/task/ExecuteQueueTask.java
@@ -0,0 +1,45 @@
+package org.jingtao8a.web.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jingtao8a.component.RedisComponent;
+import org.jingtao8a.constants.Constants;
+import org.jingtao8a.entity.po.VideoInfoFilePost;
+import org.jingtao8a.service.VideoInfoFilePostService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Component
+@Slf4j
+public class ExecuteQueueTask {
+    private ExecutorService executorService = Executors.newFixedThreadPool(Constants.LENGTH_5);
+
+    @Resource
+    private RedisComponent  redisComponent;
+
+    @Resource
+    private VideoInfoFilePostService videoInfoFilePostService;
+
+    @PostConstruct
+    public void consumTransferFileQueue() {
+        executorService.submit(()->{
+            while (true) {
+                try {
+                    VideoInfoFilePost videoInfoFilePost = redisComponent.getFileFromTransferQueue();
+                    if (videoInfoFilePost == null) {
+                        Thread.sleep(1500);
+                        continue;
+                    }
+                    videoInfoFilePostService.transferVideoFile(videoInfoFilePost);
+                } catch (Exception e) {
+                    log.error("获取转码文件队列信息失败", e);
+                }
+            }
+        });
+    }
+
+
+}
