dd78ce6294fb938889f92a51615417b2a1955ea2
yuxintao <1921056015@qq.com>
2024-12-16 15:10:44 +0800

    VideoInfo添加nickName和avatar

--- easylive-common/src/main/java/org/jingtao8a/entity/po/VideoInfo.java
+++ easylive-common/src/main/java/org/jingtao8a/entity/po/VideoInfo.java
@@ -104,4 +104,8 @@ public class VideoInfo implements Serializable {
 	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
 	@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
 	private Date lastPlayTime;
+
+	private String nickName;
+
+	private String avatar;
 }
