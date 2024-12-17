df659055c95b99de60912972ce96564eb6388817
yuxintao <1921056015@qq.com>
2024-12-17 11:55:03 +0800

    添加VideoInfoResultVO{videoInfo,userActionList}

---
+++ easylive-common/src/main/java/org/jingtao8a/vo/VideoInfoResultVO.java
@@ -0,0 +1,13 @@
+package org.jingtao8a.vo;
+
+import lombok.Data;
+import lombok.ToString;
+import org.jingtao8a.entity.po.VideoInfo;
+import java.util.*;
+
+@Data
+@ToString
+public class VideoInfoResultVO {
+    private VideoInfo videoInfo;
+    private List userActionList;
+}
