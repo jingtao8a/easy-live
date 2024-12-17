5ba4aefadcc6661dc3539059097eec9badc33dd8
yuxintao <1921056015@qq.com>
2024-12-17 15:52:15 +0800

    add /danmu下的/postDanmu和/loadDanmu

---
+++ easylive-web/src/main/java/org/jingtao8a/web/controller/VideoDanmuController.java
@@ -0,0 +1,73 @@
+package org.jingtao8a.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jingtao8a.constants.Constants;
+import org.jingtao8a.dto.TokenUserInfoDto;
+import org.jingtao8a.entity.po.VideoDanmu;
+import org.jingtao8a.entity.po.VideoInfo;
+import org.jingtao8a.entity.query.VideoDanmuQuery;
+import org.jingtao8a.enums.ResponseCodeEnum;
+import org.jingtao8a.exception.BusinessException;
+import org.jingtao8a.service.VideoDanmuService;
+import org.jingtao8a.service.impl.VideoInfoServiceImpl;
+import org.jingtao8a.vo.ResponseVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.swing.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Date;
+
+@RestController
+@Validated
+@Slf4j
+@RequestMapping("/danmu")
+public class VideoDanmuController extends ABaseController {
+    @Resource
+    private VideoDanmuService videoDanmuService;
+    @Autowired
+    private VideoInfoServiceImpl videoInfoService;
+
+    @RequestMapping("/postDanmu")
+    public ResponseVO postDanmu(@NotEmpty String videoId,
+                                @NotEmpty String fileId,
+                                @NotEmpty @Size(max=200) String text,
+                                @NotNull Integer mode,
+                                @NotEmpty String color,
+                                @NotNull Integer time) throws BusinessException {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        VideoDanmu videoDanmu = new VideoDanmu();
+        videoDanmu.setVideoId(videoId);
+        videoDanmu.setFileId(fileId);
+        videoDanmu.setText(text);
+        videoDanmu.setMode(mode);
+        videoDanmu.setColor(color);
+        videoDanmu.setTime(time);
+        videoDanmu.setUserId(tokenUserInfoDto.getUserId());
+        videoDanmu.setPostTime(new Date());
+        videoDanmuService.saveVideoDanmu(videoDanmu);
+        return getSuccessResponseVO(null);
+    }
+
+    @RequestMapping("/loadDanmu")
+    public ResponseVO loadDanmu(@NotEmpty String videoId, @NotEmpty String fileId) throws BusinessException {
+        VideoInfo videoInfo = videoInfoService.selectByVideoId(videoId);
+        if (videoInfo == null) {
+            throw new BusinessException(ResponseCodeEnum.CODE_600);
+        }
+        if (videoInfo.getInteraction() != null && videoInfo.getInteraction().contains(Constants.ONE.toString())) {
+            return getSuccessResponseVO(new ArrayList<>());
+        }
+        VideoDanmuQuery videoDanmuQuery = new VideoDanmuQuery();
+        videoDanmuQuery.setVideoId(videoId);
+        videoDanmuQuery.setOrderBy("danmu_id asc");
+        return getSuccessResponseVO(videoDanmuService.findListByParam(videoDanmuQuery));
+    }
+
+}
