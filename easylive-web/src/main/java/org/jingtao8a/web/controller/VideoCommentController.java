658df3b4f1bee3e1d9811068018fd02a78128e15
yuxintao <1921056015@qq.com>
2024-12-19 08:54:37 +0800

    add /comment/loadComment

--- easylive-web/src/main/java/org/jingtao8a/web/controller/VideoCommentController.java
+++ easylive-web/src/main/java/org/jingtao8a/web/controller/VideoCommentController.java
@@ -1,11 +1,23 @@
 package org.jingtao8a.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jingtao8a.constants.Constants;
 import org.jingtao8a.dto.TokenUserInfoDto;
+import org.jingtao8a.entity.po.UserAction;
 import org.jingtao8a.entity.po.VideoComment;
+import org.jingtao8a.entity.po.VideoInfo;
+import org.jingtao8a.entity.query.UserActionQuery;
+import org.jingtao8a.entity.query.VideoCommentQuery;
+import org.jingtao8a.enums.CommentTopTypeEnum;
+import org.jingtao8a.enums.ResponseCodeEnum;
+import org.jingtao8a.enums.UserActionTypeEnum;
 import org.jingtao8a.exception.BusinessException;
+import org.jingtao8a.service.UserActionService;
 import org.jingtao8a.service.VideoCommentService;
+import org.jingtao8a.service.VideoInfoService;
+import org.jingtao8a.vo.PaginationResultVO;
 import org.jingtao8a.vo.ResponseVO;
+import org.jingtao8a.vo.VideoCommentResultVO;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Validated
@@ -21,6 +35,12 @@ import javax.validation.constraints.Size;
 public class VideoCommentController extends ABaseController{
     @Resource
     private VideoCommentService videoCommentService;
+
+    @Resource
+    private VideoInfoService videoInfoService;
+
+    @Resource
+    private UserActionService userActionService;
 
     @RequestMapping("/postComment")
     public ResponseVO postComment(@NotEmpty String videoId,
@@ -40,5 +60,59 @@ public class VideoCommentController extends ABaseController{
         videoComment.setAvatar(tokenUserInfoDto.getAvatar());
         videoCommentService.postComment(videoComment, replyCommentId);
         return getSuccessResponseVO(videoComment);
+    }
+
+    @RequestMapping("/loadComment")
+    public ResponseVO loadComment(@NotEmpty String videoId, String pageNo, Integer orderType) throws BusinessException {
+        VideoInfo videoInfo = videoInfoService.selectByVideoId(videoId);
+        if (videoInfo == null) {
+            throw new BusinessException(ResponseCodeEnum.CODE_600);
+        }
+        if (videoInfo.getInteraction() != null && videoInfo.getInteraction().contains(Constants.ZERO.toString())) {
+            return getSuccessResponseVO(new VideoCommentResultVO());
+        }
+        VideoCommentQuery videoCommentQuery = new VideoCommentQuery();
+        videoCommentQuery.setVideoId(videoId);
+        try {
+            videoCommentQuery.setPageNo(Long.valueOf(pageNo));
+        } catch (NumberFormatException e) {
+            videoCommentQuery.setPageNo(0L);
+        }
+        videoCommentQuery.setPCommentId(0);
+        String orderBy = orderType == null || orderType == 0 ? "like_count desc, comment_id desc" : "comment_id desc";
+        videoCommentQuery.setOrderBy(orderBy);
+        videoCommentQuery.setTopType(CommentTopTypeEnum.NO_TOP.getType());
+        videoCommentQuery.setLoadChildren(true);
+        PaginationResultVO<VideoComment> commentPaginationResultVO = videoCommentService.findListByPage(videoCommentQuery);
+
+        List<VideoComment> topCommentList = topComment(videoId);//TODO 置顶评论，可能有BUG
+        if (!topCommentList.isEmpty()) {
+            List<VideoComment> commentList = commentPaginationResultVO.getList();
+            topCommentList.addAll(commentList);
+            commentPaginationResultVO.setList(topCommentList);
+        }
+        //获取用户行为 (评论喜欢、讨厌)
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        List<UserAction> userActions = new ArrayList<>();
+        if (tokenUserInfoDto != null) {//登入状态
+            UserActionQuery userActionQuery = new UserActionQuery();
+            userActionQuery.setUserId(tokenUserInfoDto.getUserId());
+            userActionQuery.setVideoId(videoId);
+            userActionQuery.setActionTypeArray(new Integer[]{UserActionTypeEnum.COMMENT_LIKE.getType(), UserActionTypeEnum.COMMENT_HATE.getType()});
+            userActions = userActionService.findListByParam(userActionQuery);
+        }
+        VideoCommentResultVO videoCommentResultVO = new VideoCommentResultVO();
+        videoCommentResultVO.setCommentData(commentPaginationResultVO);
+        videoCommentResultVO.setUserActionList(userActions);
+        return getSuccessResponseVO(videoCommentResultVO);
+    }
+
+    private List<VideoComment> topComment(String videoId) {
+        VideoCommentQuery videoCommentQuery = new VideoCommentQuery();
+        videoCommentQuery.setVideoId(videoId);
+        videoCommentQuery.setTopType(CommentTopTypeEnum.TOP.getType());
+        videoCommentQuery.setLoadChildren(true);
+        List<VideoComment> videoComments = videoCommentService.findListByParam(videoCommentQuery);
+        return videoComments;
     }
 }
