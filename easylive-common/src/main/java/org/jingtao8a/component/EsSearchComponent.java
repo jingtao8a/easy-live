3a0d4b22804dfccb3cb849100604374ed67571ea
yuxintao <1921056015@qq.com>
2024-12-24 11:21:34 +0800

    update EsSearchComponent

--- easylive-common/src/main/java/org/jingtao8a/component/EsSearchComponent.java
+++ easylive-common/src/main/java/org/jingtao8a/component/EsSearchComponent.java
@@ -1,16 +1,30 @@
 package org.jingtao8a.component;
 
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.script.Script;
+import org.elasticsearch.script.ScriptType;
 import org.elasticsearch.xcontent.XContentType;
 import org.jingtao8a.config.AppConfig;
+import org.jingtao8a.dto.VideoInfoEsDto;
+import org.jingtao8a.entity.po.VideoInfo;
 import org.jingtao8a.exception.BusinessException;
+import org.jingtao8a.utils.CopyTools;
+import org.jingtao8a.utils.JsonUtils;
+import org.jingtao8a.utils.StringTools;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.*;
 import javax.annotation.Resource;
 import java.io.IOException;
 
@@ -97,6 +111,71 @@ public class EsSearchComponent {
         } catch (Exception e) {
             log.error("初始化es失败");
             throw new BusinessException("初始化es失败");//触发destroy
+        }
+    }
+
+    public void saveDoc(VideoInfo videoInfo) throws BusinessException {
+        try {
+            if (docExist(videoInfo.getVideoId())) {//修改doc
+                updateDoc(videoInfo);
+            } else {//新增doc
+                VideoInfoEsDto videoInfoEsDto = CopyTools.copy(videoInfo, VideoInfoEsDto.class);
+                videoInfoEsDto.setCollectCount(0);
+                videoInfoEsDto.setPlayCount(0);
+                videoInfoEsDto.setDanmuCount(0);
+                IndexRequest request = new IndexRequest((appConfig.getEsIndexVideoName()));
+                request.id(videoInfoEsDto.getVideoId()).source(JsonUtils.convertObj2Json(videoInfoEsDto), XContentType.JSON);
+                restHighLevelClient.index(request, RequestOptions.DEFAULT);
+            }
+        } catch (Exception e) {
+            log.error("保存到es失败", e);
+            throw new BusinessException("保存到es失败");
+        }
+    }
+
+    private boolean docExist(String id) throws IOException {
+        GetRequest getRequest = new GetRequest(appConfig.getEsIndexVideoName(), id);
+        GetResponse getResponse = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);
+        return getResponse.isExists();
+    }
+
+    private void updateDoc(VideoInfo videoInfo) throws BusinessException {
+        try {
+            videoInfo.setLastUpdateTime(null);
+            videoInfo.setCreateTime(null);
+            Map<String, Object> dataMap = new HashMap<>();
+            Field[] fields = VideoInfo.class.getFields();
+            for (Field field : fields) {
+                String methodName = "get" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
+                Method method = VideoInfo.class.getMethod(methodName);
+                Object object = method.invoke(videoInfo);
+                if (object != null) {
+                    if (object instanceof String && StringTools.isEmpty((String) object) || !(object instanceof String)) {
+                        dataMap.put(field.getName(), object);
+                    }
+                }
+            }
+            if (dataMap.isEmpty()) {
+                return;
+            }
+            UpdateRequest updateRequest = new UpdateRequest(appConfig.getEsIndexVideoName(), videoInfo.getVideoId());
+            updateRequest.doc(dataMap);
+            restHighLevelClient.update(updateRequest, RequestOptions.DEFAULT);
+        } catch (Exception e) {
+            log.error("es更新视频失败", e);
+            throw new BusinessException("保存视频失败");
+        }
+    }
+
+    private void updateDocCount(String videoId, String fieldName, Integer count) throws BusinessException {
+        try {
+            UpdateRequest updateRequest = new UpdateRequest(appConfig.getEsIndexVideoName(), videoId);
+            Script script = new Script(ScriptType.INLINE, "painless", "ctx._source." + fieldName + " += params.count", Collections.singletonMap("count", count));
+            updateRequest.script(script);
+            restHighLevelClient.update(updateRequest, RequestOptions.DEFAULT);
+        } catch (Exception e) {
+            log.error("es更新视频失败", e);
+            throw new BusinessException("保存视频失败");
         }
     }
 }
