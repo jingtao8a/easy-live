916bebbb7defb2e094efc8ed1212c51a4d658611
yuxintao <1921056015@qq.com>
2024-12-26 21:16:11 +0800

    add /ucenter下的/getgetActualTimeStatisticsInfo和/getWeekStatisticsInfo

--- easylive-web/src/main/java/org/jingtao8a/web/controller/UcenterVideoPostController.java
+++ easylive-web/src/main/java/org/jingtao8a/web/controller/UcenterVideoPostController.java
@@ -8,11 +8,13 @@ import org.jingtao8a.entity.query.*;
 import org.jingtao8a.enums.ResponseCodeEnum;
 import org.jingtao8a.enums.VideoStatusEnum;
 import org.jingtao8a.exception.BusinessException;
+import org.jingtao8a.service.StatisticsInfoService;
 import org.jingtao8a.service.VideoInfoFilePostService;
 import org.jingtao8a.service.VideoInfoPostService;
 import org.jingtao8a.service.VideoInfoService;
 import org.jingtao8a.service.impl.VideoCommentServiceImpl;
 import org.jingtao8a.service.impl.VideoDanmuServiceImpl;
+import org.jingtao8a.utils.DateUtils;
 import org.jingtao8a.utils.JsonUtils;
 import org.jingtao8a.vo.PaginationResultVO;
 import org.jingtao8a.vo.ResponseVO;
@@ -27,6 +29,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/ucenter")
@@ -43,6 +46,8 @@ public class UcenterVideoPostController extends ABaseController{
     private VideoCommentServiceImpl videoCommentService;
     @Resource
     private VideoDanmuServiceImpl videoDanmuService;
+    @Resource
+    private StatisticsInfoService statisticsInfoService;
 
     @RequestMapping("/postVideo")
     @GlobalInterceptor(checkLogin = true)
@@ -209,5 +214,61 @@ public class UcenterVideoPostController extends ABaseController{
         TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
         videoDanmuService.deleteDanmu(danmuId, tokenUserInfoDto.getUserId());
         return getSuccessResponseVO(null);
+    }
+
+
+    @RequestMapping("/getActualTimeStatisticsInfo")
+    @GlobalInterceptor(checkLogin = true)
+    public ResponseVO getActualTimeStatisticsInfo() {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        //获取前一天的统计信息，存储在preDayDataMap中
+        String preDate = DateUtils.getPreviousDayDate(1);
+        StatisticsInfoQuery statisticsInfoQuery = new StatisticsInfoQuery();
+        statisticsInfoQuery.setUserId(tokenUserInfoDto.getUserId());
+        statisticsInfoQuery.setStatisticsDate(preDate);
+        List<StatisticsInfo> preDayStatisticsInfoList = statisticsInfoService.findListByParam(statisticsInfoQuery);
+        Map<Integer, Integer> preDayDataMap = preDayStatisticsInfoList.stream().collect(Collectors.toMap(
+                item -> item.getDataType(),
+                item -> item.getStatisticsCount(),
+                (data1, data2) -> data1
+        ));
+        //获取总的统计信息，存储在totalCountInfo中
+        Map<String, Integer> totalCountInfo = statisticsInfoService.getStatisticsInfoActualTime(tokenUserInfoDto.getUserId());
+        Map<String, Object> result = new HashMap<>();
+        result.put("preDayData", preDayDataMap);
+        result.put("totalCountInfo", totalCountInfo);
+        return getSuccessResponseVO(result);
+    }
+
+    @RequestMapping("/getWeekStatisticsInfo")
+    @GlobalInterceptor(checkLogin = true)
+    public ResponseVO getWeekStatisticsInfo(@NotNull Integer dataType) {
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        List<String> dateList = DateUtils.getPreviousDayDates(7);
+        StatisticsInfoQuery statisticsInfoQuery = new StatisticsInfoQuery();
+        statisticsInfoQuery.setUserId(tokenUserInfoDto.getUserId());
+        statisticsInfoQuery.setDataType(dataType);
+        statisticsInfoQuery.setStatisticsDateStart(dateList.get(0));
+        statisticsInfoQuery.setStatisticsDateEnd(dateList.get(dateList.size() - 1));
+        statisticsInfoQuery.setOrderBy("statistics_date asc");
+        List<StatisticsInfo> statisticsInfoList = statisticsInfoService.findListByParam(statisticsInfoQuery);
+        Map<String, StatisticsInfo> dataMap = statisticsInfoList.stream().collect(Collectors.toMap(
+                item -> item.getStatisticsDate(),
+                item -> item,
+                (data1, data2) -> data1
+        ));
+        List<StatisticsInfo> resultDataList = new ArrayList<>();
+        for (String date : dateList) {
+            StatisticsInfo statisticsInfo = dataMap.get(date);
+            if (statisticsInfo == null) {
+                statisticsInfo = new StatisticsInfo();
+                statisticsInfo.setStatisticsDate(date);
+                statisticsInfo.setStatisticsCount(0);
+                statisticsInfo.setDataType(dataType);
+                statisticsInfo.setUserId(tokenUserInfoDto.getUserId());
+            }
+            resultDataList.add(statisticsInfo);
+        }
+        return getSuccessResponseVO(resultDataList);
     }
 }
