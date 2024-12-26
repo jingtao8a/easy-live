2c1b560f87136eb8762845d81fc44a366502f328
yuxintao <1921056015@qq.com>
2024-12-26 08:33:26 +0800

    add /message下的/readAll,/loadMessage,/delMessage

--- easylive-web/src/main/java/org/jingtao8a/web/controller/UserMessageController.java
+++ easylive-web/src/main/java/org/jingtao8a/web/controller/UserMessageController.java
@@ -4,15 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.jingtao8a.annotation.GlobalInterceptor;
 import org.jingtao8a.dto.TokenUserInfoDto;
 import org.jingtao8a.dto.UserMessageCountDto;
+import org.jingtao8a.entity.po.UserMessage;
 import org.jingtao8a.entity.query.UserMessageQuery;
 import org.jingtao8a.enums.MessageReadTypeEnum;
 import org.jingtao8a.service.UserMessageService;
+import org.jingtao8a.vo.PaginationResultVO;
 import org.jingtao8a.vo.ResponseVO;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
 import java.util.*;
 
 @RestController
@@ -40,5 +43,45 @@ public class UserMessageController extends ABaseController{
         TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
         List<UserMessageCountDto> dataList = userMessageService.getMessageTypeNoReadCount(tokenUserInfoDto.getUserId());
         return getSuccessResponseVO(dataList);
+    }
+
+    @RequestMapping("/readAll")
+    @GlobalInterceptor(checkLogin = true)
+    public ResponseVO readAll(Integer messageType){
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        UserMessageQuery userMessageQuery = new UserMessageQuery();
+        userMessageQuery.setUserId(tokenUserInfoDto.getUserId());
+        userMessageQuery.setMessageType(messageType);
+        userMessageQuery.setReadType(MessageReadTypeEnum.NO_READ.getType());
+
+        UserMessage userMessage = new UserMessage();
+        userMessage.setReadType(MessageReadTypeEnum.READ.getType());
+        userMessageService.updateByParam(userMessage, userMessageQuery);
+        return getSuccessResponseVO(null);
+    }
+
+    @RequestMapping("/loadMessage")
+    @GlobalInterceptor(checkLogin = true)
+    public ResponseVO loadMessage(Integer pageNo, @NotNull Integer messageType){
+         TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+         pageNo = pageNo == null ? 1 : pageNo;
+         UserMessageQuery userMessageQuery = new UserMessageQuery();
+         userMessageQuery.setUserId(tokenUserInfoDto.getUserId());
+         userMessageQuery.setMessageType(messageType);
+         userMessageQuery.setPageNo((long)pageNo);
+         userMessageQuery.setOrderBy("message_id desc");
+         PaginationResultVO<UserMessage> resultVO = userMessageService.findListByPage(userMessageQuery);
+         return getSuccessResponseVO(resultVO);
+    }
+
+    @RequestMapping("/delMessage")
+    @GlobalInterceptor(checkLogin = true)
+    public ResponseVO delMessage(@NotNull Integer messageId){
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        UserMessageQuery userMessageQuery = new UserMessageQuery();
+        userMessageQuery.setUserId(tokenUserInfoDto.getUserId());
+        userMessageQuery.setMessageId(messageId);
+        userMessageService.deleteByParam(userMessageQuery);
+        return getSuccessResponseVO(null);
     }
 }
