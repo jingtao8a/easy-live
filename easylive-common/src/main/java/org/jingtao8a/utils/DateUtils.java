54371203bbc0d06184d9b5935fbbf36375f9d4c2
yuxintao <1921056015@qq.com>
2024-12-26 21:10:27 +0800

    DateUtils add getPreviousDayDate and getPreviousDayDates

--- easylive-common/src/main/java/org/jingtao8a/utils/DateUtils.java
+++ easylive-common/src/main/java/org/jingtao8a/utils/DateUtils.java
@@ -2,7 +2,7 @@ package org.jingtao8a.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 public class DateUtils {
     public static String YYYYMM = "yyyyMM";
@@ -19,5 +19,19 @@ public class DateUtils {
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String getPreviousDayDate(Integer day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -day);
+        return format(calendar.getTime(), YYYY_MM_DD);
+    }
+
+    public static List<String> getPreviousDayDates(Integer day) {
+        List<String> dateList = new ArrayList<>();
+        for (int i = day; i > 0; --i) {
+            dateList.add(getPreviousDayDate(i));
+        }
+        return dateList;
     }
 }
