45e05deece35496b1d480938be19caefa9d38116
yuxintao <1921056015@qq.com>
2024-12-27 10:09:29 +0800

    admin端 add /file下的/videoResource/{fileId}，/videoResource/{fileId}/{ts}

--- easylive-admin/src/main/java/org/jingtao8a/admin/controller/FileController.java
+++ easylive-admin/src/main/java/org/jingtao8a/admin/controller/FileController.java
@@ -3,13 +3,16 @@ package org.jingtao8a.admin.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.jingtao8a.config.AppConfig;
 import org.jingtao8a.constants.Constants;
+import org.jingtao8a.entity.po.VideoInfoFilePost;
 import org.jingtao8a.enums.ResponseCodeEnum;
 import org.jingtao8a.exception.BusinessException;
+import org.jingtao8a.service.VideoInfoFilePostService;
 import org.jingtao8a.utils.DateUtils;
 import org.jingtao8a.utils.FFmpegUtils;
 import org.jingtao8a.utils.StringTools;
 import org.jingtao8a.vo.ResponseVO;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +24,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.*;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @RestController
@@ -33,6 +37,9 @@ public class FileController extends ABaseController{
 
     @Resource
     private FFmpegUtils fFmpegUtils;
+
+    @Resource
+    private VideoInfoFilePostService videoInfoFilePostService;
 
     @RequestMapping("/uploadImage")
     public ResponseVO uploadImage(@NotNull MultipartFile file, @NotNull Boolean createThumbnail) throws IOException, BusinessException {
@@ -82,5 +89,25 @@ public class FileController extends ABaseController{
         } catch (Exception e) {
             log.error("读取文件异常", e);
         }
+    }
+
+    @RequestMapping("/videoResource/{fileId}")
+    public void videResource(@PathVariable @NotEmpty String fileId, HttpServletResponse response) throws BusinessException {
+        VideoInfoFilePost videoInfoFilePost = videoInfoFilePostService.selectByFileId(fileId);
+        if (videoInfoFilePost == null) {
+            throw new BusinessException(ResponseCodeEnum.CODE_600);
+        }
+        String filePath = videoInfoFilePost.getFilePath();
+        readFile(response, filePath + "/" + Constants.M3U8_NAME);
+    }
+
+    @RequestMapping("/videoResource/{fileId}/{ts}")
+    public void videResourceTS(@PathVariable @NotEmpty String fileId, @PathVariable @NotEmpty String ts, HttpServletResponse response) throws BusinessException {
+        VideoInfoFilePost videoInfoFilePost = videoInfoFilePostService.selectByFileId(fileId);
+        if (videoInfoFilePost == null) {
+            throw new BusinessException(ResponseCodeEnum.CODE_600);
+        }
+        String filePath = videoInfoFilePost.getFilePath();
+        readFile(response, filePath + "/" + ts);
     }
 }
